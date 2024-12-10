9b52c0c2df0f5c5fceed39e347e73afa5371fade
1Murphy1 <115133958+1Murphy1@users.noreply.github.com>
2024-12-10 23:37:30 +0700

    add redux for add Task

--- src/components/taskForm.jsx
+++ src/components/taskForm.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addTask } from '../redux/taskSlice';
 
-function TaskForm({ title, setTitle, about, setAbout, onAddTask }) {
+function TaskForm({ title, setTitle, about, setAbout }) {
+    const dispatch = useDispatch();
+
+    const handleAddTask = () => {
+        if (title.trim() && about.trim()) {
+            dispatch(addTask({ title: title.trim(), about: about.trim() }));
+            setTitle('');
+            setAbout('');
+        } else {
+            alert('The fields should not be empty.');
+        }
+    };
+
     return (
         <div className="createTaskContainer-common">
             <div className="createTaskContainer-common-input">
-                <input className="yellowOutline" placeholder="Title..." value={title} onChange={(e) => setTitle(e.target.value)}/>
-                <input className="yellowOutline" placeholder="About..." value={about} onChange={(e) => setAbout(e.target.value)}/>
+                <input className="yellowOutline" placeholder="Title..." value={title} onChange={(e) => setTitle(e.target.value)} />
+                <input className="yellowOutline" placeholder="About..." value={about} onChange={(e) => setAbout(e.target.value)} />
             </div>
-            <button className="yellowOutline" onClick={onAddTask}>+</button>
+            <button className="yellowOutline" onClick={handleAddTask}>+</button>
         </div>
     );
 }
